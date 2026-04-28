eb83c463033e8469765a3662ba412a6243b30313
CJH104 <574026599@qq.com>
2026-04-28 16:13:31 +0800

    Update Argument

--- KeypointDetection/inference.py
+++ KeypointDetection/inference.py
@@ -7,11 +7,11 @@ from pathlib import Path
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Thyroid Keypoint Detection Inference')
-    parser.add_argument('--weights', type=str, default=r'D:\Code\机器人\IS-MAN-main\KeypointDetection\checkpoint\yolo11m_pose_best_thy_ketpoint.pt',
+    parser.add_argument('--weights', type=str, default='KeypointDetection\checkpoint\yolo11m_pose_best_thy_ketpoint.pt',
                         help='Path to pretrained YOLO-Pose weights')
-    parser.add_argument('--img_path', type=str, default=r'D:\Code\机器人\IS-MAN-main\KeypointDetection\demo\image\test2.jpg',
+    parser.add_argument('--img_path', type=str, default='KeypointDetection\demo\image\test1.jpg',
                         help='Path to input ultrasound image')
-    parser.add_argument('--save_path', type=str, default=r'D:\Code\机器人\IS-MAN-main\KeypointDetection/output/thyroid_keypoint_result2.jpg',
+    parser.add_argument('--save_path', type=str, default='KeypointDetection/output/thyroid_keypoint_result1.jpg',
                         help='Path to save output result')
     parser.add_argument('--device', type=str, default='cuda',
                         help='Inference device: cuda or cpu')
